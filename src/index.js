89138a6180b73972bac848db14aa297a2d4fc255
raido kaasik <raido.kaasik@outlook.com>
2020-11-30 13:34:30 +0200

    Deploy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Containers/App.js";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path="/">
-        <App />
-      </Route>
-    </Switch>
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
