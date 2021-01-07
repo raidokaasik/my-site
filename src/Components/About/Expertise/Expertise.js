68b6da9c7e18b17399e1a881fb905a228729d901
vattmees <raido.kaasik@gmail.com>
2021-01-07 17:23:05 +0200

    expertise

---
+++ src/Components/About/Expertise/Expertise.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Expertise.css";
+import Card from "../Card/Card.js";
+
+const expertise = () => {
+  return (
+    <div className="expertise-container">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default expertise;
