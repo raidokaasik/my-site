a326840da38f466b6c856ffeef6d784c000cfcb4
raido kaasik <raido.kaasik@outlook.com>
2021-01-07 20:11:33 +0200

    merge

--- src/Components/Projects/Graphics/Graphics.js
+++ src/Components/Projects/Graphics/Graphics.js
@@ -1,94 +1,16 @@
-<<<<<<< HEAD
-import React from "react";
-import GraphicsData from "../../../Data/GraphicsData.js";
-import Graphics07 from "../../../img/Graphics/Graphics_07.jpg";
-import Graphics01 from "../../../img/Graphics/Graphics_01.jpg";
-import Graphics08 from "../../../img/Graphics/Graphics_08.jpg";
-import Graphics03 from "../../../img/Graphics/Graphics_03.jpg";
-import Graphics04 from "../../../img/Graphics/Graphics_04.jpg";
-import Graphics05 from "../../../img/Graphics/Graphics_05.jpg";
-import Graphics06 from "../../../img/Graphics/Graphics_06.jpg";
-=======
-import React, { Fragment, useState, useEffect } from "react";
->>>>>>> 910ba281090f131da22b0256ce1d084ad71e24b9
+import React, {Fragment, useState, useEffect} from "react";
 import "./Graphics.css";
 import GraphicsData from "../../../Data/imageData.js";
-import Backdrop from "../../UI/Backdrop/Backdrop.js";
-import Modal from "../../UI/Modal/Modal.js";
+import Backdrop from "../../../Components/Ui/Backdrop/Backdrop.js";
+import Modal from "../../../Components/Ui/Modal/Modal.js";
 
-<<<<<<< HEAD
-const graphics = props => {
-  const images = require("./img/Graphics/Graphics_01.jpg");
-  // const images = GraphicsData.map(item => {
-  //   return (
-  //     <div
-  //       onClick={props.clicked}
-  //       className="image-3d"
-  //       style={{background: "blue"}}
-  //     >
-  //       <img src={require(`./${item.name}.jpg`)} alt="Nice picture"></img>
-  //     </div>
-  //   );
-  // });
-  return (
-    <div className="graphics-content">
-      {/* <div className="graphics-content-info">
-        <h4>Graphics projects</h4>
-        <hr />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-          repudiandae sapiente ex aliquam quibusdam similique vero recusandae
-          consequatur minus iusto corporis!
-        </p>
-      </div> */}
-      <div className="graphics-content-items">
-        {images}
-        {/* <h1>Coming soon!</h1> */}
-
-        {/* {GraphicsData.map(item => {
-          return (
-            <div
-              onClick={props.clicked}
-              className="image-3d"
-              style={{background: "blue"}}
-            >
-              <img
-                src={require(`./Graphics/${item.name}.jpg`)}
-                alt="Nice picture"
-              ></img>
-            </div>
-          );
-        })} */}
-
-        {/* <div className="image-3d" style={{background: "brown"}}>
-          <img src={Graphics08} alt="Nice picture"></img>
-        </div>
-        <div className="image-3d" style={{background: "yellow"}}>
-          <img src={Graphics03} alt="Nice picture"></img>
-        </div>
-        <div className="image-3d" style={{background: "red"}}>
-          <img src={Graphics04} alt="Nice picture"></img>
-        </div>
-        <div className="image-3d" style={{background: "gray"}}>
-          <img src={Graphics05} alt="Nice picture"></img>
-        </div>
-        <div className="image-3d" style={{background: "purple"}}>
-          <img src={Graphics06} alt="Nice picture"></img>
-        </div>
-        <div className="image-3d" style={{background: "green"}}>
-          <img src={Graphics01} alt="Nice picture"></img>
-        </div>
-        <div className="image-3d" style={{background: "lightBlue"}}>
-          black
-        </div> */}
-=======
 function Graphics(props) {
   const [currentImage, setCurrentImage] = useState({});
   const [modal, setModal] = useState(false);
   const [backDrop, setBackDrop] = useState(false);
   const [count, setCount] = useState(0);
 
-  const modalHandler = (item) => {
+  const modalHandler = item => {
     setModal(!modal);
     setBackDrop(!backDrop);
     setCurrentImage(item);
@@ -99,7 +21,7 @@ function Graphics(props) {
   const prevImageHandler = () => {
     if (currentImage.nr > 1) {
       setCurrentImage(
-        GraphicsData.filter((item) => {
+        GraphicsData.filter(item => {
           return (
             item.nr < GraphicsData.length && item.nr === currentImage.nr - 1
           );
@@ -112,7 +34,7 @@ function Graphics(props) {
   const nextImageHandler = () => {
     if (currentImage.nr !== GraphicsData.length) {
       setCurrentImage(
-        GraphicsData.filter((item) => {
+        GraphicsData.filter(item => {
           return (
             item.nr < GraphicsData.length + 1 && item.nr === currentImage.nr + 1
           );
@@ -176,7 +98,6 @@ function Graphics(props) {
           <hr />
         </div> */}
         <div className="graphics-content-items">{images}</div>
->>>>>>> 910ba281090f131da22b0256ce1d084ad71e24b9
       </div>
     </Fragment>
   );
--- src/Components/Projects/ProjectContainer.js
+++ src/Components/Projects/ProjectContainer.js
@@ -1,17 +1,10 @@
-import React, { Component, Fragment } from "react";
+import React, {Component, Fragment} from "react";
 import "./ProjectContainer.css";
 import Graphics from "./Graphics/Graphics.js";
 import Coding from "./Coding/Coding.js";
 import Ui from "./Ui/Ui.js";
-<<<<<<< HEAD
-import Modal from "../Ui/Modal/Modal.js";
-import Data from "../../Data/Data.js";
-import Item from "./Coding/Item/Item.js";
-import ExpandedItem from "./Coding/Item/ExpandedItem/ExpandedItem.js";
-=======
 import Dot from "./UI elements/Dot/Dot.js";
 import Line from "./UI elements/Line/BreakLine.js";
->>>>>>> 910ba281090f131da22b0256ce1d084ad71e24b9
 
 class ProjectContainer extends Component {
   state = {
@@ -24,18 +17,18 @@ class ProjectContainer extends Component {
   };
 
   graphicsHandler = () => {
-    this.setState({ graphics: true, coding: false, ui: false });
+    this.setState({graphics: true, coding: false, ui: false});
   };
   codingHandler = () => {
-    this.setState({ graphics: false, coding: true, ui: false });
+    this.setState({graphics: false, coding: true, ui: false});
   };
   uiHandler = () => {
-    this.setState({ graphics: false, coding: false, ui: true });
+    this.setState({graphics: false, coding: false, ui: true});
   };
 
   modalHandler = () => {
-    this.setState({ modal: !this.state.modal });
-    this.setState({ backDrop: !this.state.backDrop });
+    this.setState({modal: !this.state.modal});
+    this.setState({backDrop: !this.state.backDrop});
   };
 
   modalHandler = () => {
@@ -84,53 +77,11 @@ class ProjectContainer extends Component {
                 <Line />
               </div>
             </div>
-<<<<<<< HEAD
-            {this.state.modal ? (
-              <Modal>
-                <div>
-                  <h1>Hello</h1>
-                </div>
-              </Modal>
-            ) : null}
-            {/* <div className="project-break"></div> */}
-=======
->>>>>>> 910ba281090f131da22b0256ce1d084ad71e24b9
             {this.state.graphics ? (
               <Graphics clicked={this.modalHandler} />
             ) : null}
             {this.state.coding ? <Coding /> : null}
             {this.state.ui ? <Ui /> : null}
-<<<<<<< HEAD
-
-            {/* {this.state.expanded ? (
-              <div className="expanded-content">
-                <ExpandedItem clicked={this.expandedItemHandler} />
-              </div>
-            ) : (
-              <div className="project-content">
-                <div className="project-content-info">
-                  <h3>Project description</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-                    id corrupti ipsam, voluptates sed aut mollitia magni nulla
-                    repudiandae sapiente ex aliquam quibusdam similique vero
-                    recusandae consequatur minus iusto corporis!
-                  </p>
-                </div>
-                <div className="project-content-items">
-                  <Item clicked={this.itemHandler} />
-                  <Item />
-                  <Item />
-                  <Item />
-                  <Item />
-                  <Item />
-                  <Item />
-                  <Item />
-                </div>
-              </div>
-            )} */}
-=======
->>>>>>> 910ba281090f131da22b0256ce1d084ad71e24b9
           </div>
         </div>
       </div>
